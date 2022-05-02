7249545777a664c3b105f60a8e8cf0064fdd6ebe
SADEK HOSSAIN <shadeks16@gmail.com>
2022-05-02 21:02:41 +0600

    delivered button done

--- src/components/Pages/InventoryItemDetails/InventoryItemDetails.js
+++ src/components/Pages/InventoryItemDetails/InventoryItemDetails.js
@@ -1,6 +1,4 @@
-import { Button } from "bootstrap";
 import React, { useEffect, useState } from "react";
-import { Card } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 
 const InventoryItemDetails = () => {
@@ -11,6 +9,15 @@ const InventoryItemDetails = () => {
       .then((res) => res.json())
       .then((data) => setProductDetail(data));
   }, []);
+
+  const delivered = () =>{
+    const url = `http://localhost:5000/product/${productDetail._id}`;
+    fetch(url,{
+        method:'PUT',
+    })
+    .then(res => res.json())
+    .then(data => setProductDetail(data))
+  }
   return (
     <div className="">
       <div className="card w-50 mx-auto" >
@@ -18,10 +25,10 @@ const InventoryItemDetails = () => {
         <div className="card-body">
         <h2 className="card-title">{productDetail.name}</h2>
           <p className="card-text">Price: {productDetail.price}</p>
-          <p className="card-text">Price: {productDetail.quantity}</p>
-          <p className="card-text">Price: {productDetail.sname}</p>
-          <p className="card-text">Price: {productDetail.description}</p>
-          <button className="btn btn-danger">Delivered</button>
+          <p className="card-text">Quantity: {productDetail.quantity}</p>
+          <p className="card-text">Supplier Name: {productDetail.sname}</p>
+          <p className="card-text">{productDetail.description}</p>
+          <button onClick={delivered} className="btn btn-danger">Delivered</button>
         </div>
       </div>
     </div>
